a09634b4f58c452bf35715414c0aeaa745812d31
THANUSHKIRAN <thanushkiran@yahoo.com>
2023-04-27 19:23:49 +0530

    ecr change

--- vars/dockerImageScanECR.groovy
+++ vars/dockerImageScanECR.groovy
@@ -1,7 +1,7 @@
 def call(String aws_acc_id,String region,String ecr_reponame){
 
     sh """
-        trivy image ${ecr_reponame}:latest ${aws_acc_id}.dkr.ecr.${region}/${ecr_reponame}:latest > scan.txt
+        trivy image ${aws_acc_id}.dkr.ecr.${region}/${ecr_reponame}:latest > scan.txt
         cat scan.txt
 
     """
