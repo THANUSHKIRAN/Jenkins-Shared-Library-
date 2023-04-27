22b0b488302ff08f72cb503a0515a1c0c7566e5b
THANUSHKIRAN <thanushkiran@yahoo.com>
2023-04-27 17:41:57 +0530

    dockerbuild

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,9 @@
+def call(String project,String Imagetag,String hubUser){
+
+    sh """
+        docker image build -t ${hubUser}/${project} .
+        docker image tag ${hubUser}/${project} ${hubUser}/${project}:${Imagetag}
+        docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+
+    """
+}
