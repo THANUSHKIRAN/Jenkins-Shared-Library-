b1b8a072b626009ce4fad1751a48b758965174bb
THANUSHKIRAN <thanushkiran@yahoo.com>
2023-04-27 18:20:55 +0530

    docker image cleanup

---
+++ vars/dockerImageCleanup.groovy
@@ -0,0 +1,8 @@
+def call(String project,String Imagetag,String hubUser){
+
+    sh """
+        docker rmi ${hubUser}/${project}:${Imagetag}
+        docker rmi ${hubUser}/${project}:latest
+
+    """
+}
